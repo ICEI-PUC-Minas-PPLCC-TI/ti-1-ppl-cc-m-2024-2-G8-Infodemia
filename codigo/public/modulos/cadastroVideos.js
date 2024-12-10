0da2b2ce6e040ae352c21adb19a0387458c70aa0
Guilherme Henrique Soares França <ghsfranca@gmail.com>
2024-12-09 23:18:38 -0300

    detalhesfinais + algumas funções

---
+++ codigo/public/modulos/cadastroVideos.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('video-form');
+
+// envio do formulário
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Pega os dados do formulário
+    const title = document.getElementById('title').value;
+    const url = document.getElementById('url').value;
+    const description = document.getElementById('description').value;
+    const tags = document.getElementById('tags').value.split(',').map(tag => tag.trim());
+
+    // video novo
+    const newVideo = {
+        title,
+        url,
+        description,
+        tags
+    };
+
+    // Envia pro JSON Server
+    fetch('http://localhost:3000/videos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newVideo)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('errro ao adicionar video');
+        }
+        alert('Vídeo adicionado com sucesso!');
+        form.reset(); // limpa o formulário
+    })
+    .catch(error => console.error('Erro:', error));
+});
+
